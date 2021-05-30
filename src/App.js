09fb1527426c2b496b8c89b037633cf237f208d7
uderek1 <duzoh@dons.usfca.edu>
2021-05-30 02:36:30 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import picture from './pictureicon.png';
 import './App.css';
-
+import React, { useState } from 'react';
+import heart from './heart.jpg';
+import linkedin from './linkedin.png';
 function App() {
+  const[writing,setWriting]=useState("My Name Is Derek");
+  const handleName=()=>{
+    setWriting("My Name Is Derek");
+  }
+  const handleSkills=()=>{
+    setWriting("I'm a Frontend Developer");
+  }
+  const[count,updateCount]=useState(0);
+  const handleCount=()=>{
+    updateCount(count+1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div class="container">
+        <div id="s1">
+          <img src={picture} alt="picture" width="150" height="150"/>
+        </div>
+        <div id="s2">
+          <h1><strong>Hi!</strong></h1>
+          <p>{writing}</p>
+        </div>
+        <div id="s3">
+          <button id="name" onClick={handleName}>Name</button>
+          &nbsp;
+          &nbsp;
+          <button id="skills" onClick={handleSkills}>Skills</button>
+        </div>
+        <p></p>
+        <div id="s4">
+          <span id="display">{count}</span>
+          &nbsp;
+         <button id="hearts" onClick={handleCount}><img src={heart} width="50" height="50"/></button>
+        </div>
+        <div id="s5">
+        <a href='https://www.linkedin.com/in/derekuzoh/'><button id="linkedin"><img src={linkedin} width="75" height="75"/></button></a>
+        </div>
+      </div>
+    </body>
   );
 }
 
